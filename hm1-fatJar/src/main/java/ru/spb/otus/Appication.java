2cf25b52741dba49edbd7a8f54e7f522bfe516d9
v_aleksandrov <aleksandrov.vetall@yandex.ru>
2017-12-05 23:11:02 +0300

    homework №1, build  fatJar project

---
+++ hm1-fatJar/src/main/java/ru/spb/otus/Appication.java
@@ -0,0 +1,10 @@
+package ru.spb.otus;
+
+/**
+ * Created by avetall  05.12.17.
+ */
+public class Appication {
+    public static void main(String[] args) {
+        System.out.println("Hello from fatJar");
+    }
+}
