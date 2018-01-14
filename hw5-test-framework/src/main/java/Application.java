637212c36dca8a4c55b55379f7b9c721d5bd18d9
v_aleksandrov <aleksandrov.vetall@yandex.ru>
2018-01-14 19:41:19 +0300

    remove garbage imports

--- hw5-test-framework/src/main/java/Application.java
+++ hw5-test-framework/src/main/java/Application.java
@@ -1,8 +1,3 @@
-import framework.reflection.ReflectionHelper;
-
-import java.io.IOException;
-import java.lang.reflect.*;
-
 /**
  * Created by avetall  10.01.18.
  */
--- hw5-test-framework/src/main/java/TestRunner.java
+++ hw5-test-framework/src/main/java/TestRunner.java
@@ -10,7 +10,6 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
